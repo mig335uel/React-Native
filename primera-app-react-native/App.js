c0ec9d781f681b3a34154fc867f7aadcd5979c69
Miguel Angel <miguelangel.gomez.05@usal.es>
2025-07-18 15:11:33 +0200

    create the Main.jsx and update de App.js

--- primera-app-react-native/App.js
+++ primera-app-react-native/App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableNativeFeedback>
+        <Text onPress={() => {
+          Alert.alert('Hello', 'Welcome to the Primera App!');
+        }}>Open up App.js to start working on your app!</Text>
+
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
     </View>
   );
